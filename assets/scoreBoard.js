9da863e0e5268d00e0722292896c39b3c3582367
Lydia <76062539+lshim98@users.noreply.github.com>
2021-02-22 23:46:59 -0500

    Update scoreBoard.js

--- assets/scoreBoard.js
+++ assets/scoreBoard.js
@@ -47,8 +47,8 @@ function renderScores() {
 
 clearScores.addEventListener("click", function () {
     clearAll();
-    window.location.href = "lshim98.github.io/codequiz.github.io/highScores.html";
+    window.location.href = "https://github.com/lshim98.github.io/codequiz.github.io/highScores.html";
 })
 backButton.addEventListener("click", function () {
-    window.location.href = "lshim98.github.io/codequiz.github.io/index.html";
+    window.location.href = "https://github.com/lshim98.github.io/codequiz.github.io/index.html";
 })
