f6e6e9a76666eb16c6de0d5f555cbcbe86aced1f
Lydia <76062539+lshim98@users.noreply.github.com>
2021-02-23 00:21:00 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -53,7 +53,7 @@ function questionEnder() {
         highScoreText.name = inputTag.value.trim();
         highScoreText.newScore = score;
         storeScores(highScoreText);
-        window.location.href = "{{ ('https://lshim98.github.io/codequiz.github.io/highScores.html') }}";
+        window.location.href = "https://lshim98.github.io/codequiz.github.io/highScores.html";
     });
 }
 function questionSetter() {
